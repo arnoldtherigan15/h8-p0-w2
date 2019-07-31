bc43713f02dbda04ab43e9cc6c3ebaf7a6f3ef46
arnold_therigan <arnoldtherigan15@gmail.com>
2019-07-31 23:29:01 +0700

    add excercise-12

---
+++ excercise-12.js
@@ -0,0 +1,13 @@
+function konversiMenit(num) { 
+    var jam = Math.floor(num / 60);    
+    var menit = num % 60;
+    if ( menit < 10) {
+        menit = '0' + num % 60;
+    } return jam + ":" + menit;  
+}
+
+console.log(konversiMenit(63));
+console.log(konversiMenit(124));
+console.log(konversiMenit(53));
+console.log(konversiMenit(88));
+console.log(konversiMenit(120));
